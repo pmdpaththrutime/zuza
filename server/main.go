dfd2b74a691b32c6f24f12251b7d52574a23beca
Evelyn <evelyn.ciara.345@gmail.com>
2026-08-21 17:10:16 -0400

    Set up getRecordingRoom handler

--- server/main.go
+++ server/main.go
@@ -12,17 +12,29 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "This is my website!\n")
 }
+
 func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func getRecordingRoom(w http.ResponseWriter, r *http.Request) {
+	room_id := r.PathValue("room")
+	if room_id != "" {
+		fmt.Fprintf(w, "You are in recording room %s\n", room_id)
+	} else {
+		// Unreachable
+		http.Error(w, "A room ID is required", http.StatusBadRequest)
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
+	mux.HandleFunc("/record/{room}", getRecordingRoom)
 
-	err := http.ListenAndServe(":3333", nil)
+	err := http.ListenAndServe(":3333", mux)
 
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
