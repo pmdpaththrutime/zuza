9d043e3f3d9f758861f7d3ecc65f32ee718cb78f
Evelyn <evelyn.ciara.345@gmail.com>
2026-08-24 20:06:56 -0400

    Implement model types

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module zuza/server
 
 go 1.27.0
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ server/models.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID       uuid.UUID
+	Username string
+	Nickname string
+}
+
+type RecordingRoom struct {
+	ID         uuid.UUID
+	RoomCode   string
+	Owner      User
+	Performers []*User // the users authorized to access this room
+}
+
+type Recording struct {
+	ID         uuid.UUID
+	Room       RecordingRoom
+	Performers []*User
+	Tracks     []*Track
+}
+
+type Track struct {
+	ID        uuid.UUID
+	Performer User
+	File      []*AudioFile // one or more (but usually one) files that represent this track
+}
+
+type AudioFile struct {
+	ID       uuid.UUID
+	MimeType string
+	Location string
+}
